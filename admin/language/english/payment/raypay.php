2a37e7de634bfe8a06cda91ce265f22cf5157d02
raypay-gateway <hanieh.ramzanpour@gmail.com>
2021-07-05 09:57:20 +0430

    first commit

---
+++ admin/language/english/payment/raypay.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * RayPay payment gateway
+ *
+ * @developer hanieh729
+ * @publisher RayPay
+ * @copyright (C) 2021 RayPay
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+ *
+ * https://raypay.ir
+ */
+$_['text_title'] = 'RayPay';
+$_['heading_title'] = 'درگاه پرداخت رای پی';
+$_['text_payment'] = 'پرداخت';
+$_['text_extension'] = 'افزونه ها';
+$_['text_success'] = 'ماژول پرداخت با موفقیت ویرایش شد!';
+$_['text_raypay'] = '<a target="_blank" href="https://raypay.ir/"><img src="view/image/payment/raypay.png" alt="RayPay" title="RayPay" style="width: 120px"></a>';
+$_['text_edit'] = 'ویرایش درگاه پرداخت رای پی';
+$_['tab_general'] = 'عمومی';
+$_['entry_title'] = 'عنوان';
+$_['text_user_id'] = 'شناسه کاربری';
+$_['text_acceptor_code'] = 'کد پذیرنده';
+$_['text_order_status'] = 'وضعیت سفارش پس از پرداخت موفق';
+$_['entry_payment_failed_message_default'] = 'پرداخت شما ناموفق بوده است. لطفا مجددا تلاش نمایید یا در صورت بروز اشکال با مدیر سایت تماس بگیرید.';
+$_['entry_payment_successful_message_default'] = 'پرداخت شما با موفقیت انجام شد';
+$_['text_successful_message_help'] = 'متن پیامی که می خواهید به کاربر نمایش دهید را وارد کنید. همچنین می توانید از شورت کد {invoice_id} برای نمایش شناسه ارجاع بانکی رای پی استفاده نمایید.';
+$_['text_failed_message_help'] = 'متن پیامی که می خواهید به کاربر نمایش دهید را وارد کنید. همچنین می توانید از شورت کد {invoice_id} برای نمایش شناسه ارجاع بانکی رای پی استفاده نمایید.';
+$_['failed_message_label'] = 'پیام پرداخت ناموفق';
+$_['successful_message_label'] = 'پیام پرداخت موفق';
+$_['text_status'] = 'وضعیت درگاه';
+$_['text_sort_order'] = 'ترتیب';
+$_['error_permission'] = 'شما اجازه ویرایش ماژول رای پی رای ندارید !';
+$_['error_validate'] = 'متاسفانه خطایی در مقدار دهی فیلد ها رخ داده است.';
+$_['error_user_id'] = 'شناسه کاربری مورد نیاز است';
+$_['error_acceptor_code'] = 'کد پذیرنده مورد نیاز است';
